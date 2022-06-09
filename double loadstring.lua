3d707488a8757eeef4da0b9b7dbcb56f8809e733
TheFatGamer123 <95467581+TheFatGamer123@users.noreply.github.com>
2022-06-09 19:40:03 +0800

    Update double loadstring.lua

--- double loadstring.lua
+++ double loadstring.lua
@@ -1,4 +1,11 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/RoBeats/AccuracyHelper.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/RoBeats/Autoplayer.lua"))()
+loadstring(
+    game:HttpGet(
+        "https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/RoBeats/AccuracyHelper.lua"
+    )
+)()
+wait(0.1)
+loadstring(
+    game:HttpGet("https://raw.githubusercontent.com/Introvert1337/RobloxReleases/main/Scripts/RoBeats/Autoplayer.lua")
+)()
 wait(0.1)
 print("executed")
